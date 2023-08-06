a48058ffc8035c794c7931bb2d659ac88fcf27cf
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-08-06 12:20:26 -0700

    setup sanity

--- client/src/app/api/fetch-all-products/route.ts
+++ client/src/app/api/fetch-all-products/route.ts
@@ -1,14 +1,12 @@
-import { client } from '@/utils/client'
-import { getAllImgGallery } from '@/utils/queries'
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import { client } from '@/utils/client'
+// import { getAllImgGallery } from '@/utils/queries'
+import { NextResponse } from 'next/server'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'GET') {
-    const query = getAllImgGallery()
-    const data = await client.fetch(query)
-    res.status(200).json(data)
-  }
+export async function GET() {
+  // if (req.method === 'GET') {
+  //   const query = getAllImgGallery()
+  //   const data = await client.fetch(query)
+  //   res.status(200).json(data)
+  // }
+  return NextResponse.json({ title: 'hello' }, { status: 200 })
 }
