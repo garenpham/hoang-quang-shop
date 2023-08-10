c639b7eb92b4e9aa7614c8646c2081e0ced815ed
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-08-09 23:03:01 -0700

    fetch from database

--- client/src/app/api/home/route.ts
+++ client/src/app/api/home/route.ts
@@ -3,6 +3,8 @@ import { getHomeContent } from '@/utils/queries'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const data = await client.fetch(getHomeContent())
+  const data = await client.fetch(getHomeContent(), {
+    next: { revalidate: 0 },
+  })
   return NextResponse.json(data, { status: 200 })
 }
